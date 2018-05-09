040b086175f2cb2212f3cae5962f4043c50ffa7f
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-09 14:59:00 +0700

    using dictionaries in Python

---
+++ Easy/08-dictionaries.py
@@ -0,0 +1,21 @@
+# create list of things around you and the their price using dictionaries
+
+items_price = {
+    "smartphone": 100,
+    "ipad": 300,
+    "laptop": 1000,
+    "backpack": 30
+}
+
+# to get the value using get method and passing in the key
+print(items_price.get("smartphone"))
+
+# get all keys
+print(items_price.keys())
+
+# add key and value
+items_price["pouch"] = 25
+
+
+
+
